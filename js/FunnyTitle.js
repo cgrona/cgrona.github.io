eae8b212adf2656305f8d02141ea63a10286744a
cgrona <1549692438@qq.com>
2022-01-11 09:57:23 +0800

    Site updated: 2022-01-11 09:57:21

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = 'Σ(っ °Д °;)っ 你要走嘛我好伤心！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '✧(◍˃̶ᗜ˂̶◍)✩ 哇喔！欢迎回来~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
