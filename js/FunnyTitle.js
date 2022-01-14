20bc5f0825a17babd638c2f5df92faffb2f3e0b3
cgrona <1549692438@qq.com>
2022-01-14 17:39:59 +0800

    Site updated: 2022-01-14 17:39:58

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/images/hutao.ico");
+        $('[rel="icon"]').attr('href', "/images/hutao.png");
         document.title = 'Σ(っ °Д °;)っ 你要走嘛我好伤心！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/images/hutao.ico");
+        $('[rel="icon"]').attr('href', "/images/hutao.png");
         document.title = '✧(◍˃̶ᗜ˂̶◍)✩ 哇喔！欢迎回来~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
